983bd865d39f0d3e367fda4326217c94ffce956b
PabloRafael-coder <pabloofael@gmail.com>
2024-06-28 08:40:57 -0300

    Created database migration.

--- src/database/migrations/20240622010038-create-user.js
+++ src/database/migrations/20240622010038-create-user.js
@@ -2,21 +2,26 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUUI,
+        AllowNull: false
+      },
+      name: Sequelize.STRING,
+      age: Sequelize.INTEGER,
+      email: Sequelize.STRING,
+      skill: Sequelize.STRING
+
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  // async down(queryInterface, Sequelize) {
+  //   /**
+  //    * Add reverting commands here.
+  //    *
+  //    * Example:
+  //    * await queryInterface.dropTable('users');
+  //    */
+  // }
 };
